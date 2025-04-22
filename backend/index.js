f195c2ac2d345cab67de5da24cc99b5faaf16f12
fool <evedansh123@gmail.com>
2025-04-22 12:18:04 +0530

    :p

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/", battleRoutes);
+app.use("/api", battleRoutes);
 app.get("/test", (req, res) => {
   res.json({ message: "API is working!" });
 });
